995b48a7566f7018e60208d5b6879c1333ec352e
Benjamin Höglinger <nefarius@dhmx.at>
2011-05-15 16:35:02 +0000

    Erstellt

---
+++ AnnoyingFlooder/AnnoyingFlooder.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace AnnoyingFlooder
+{
+    partial class AnnoyingFlooder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFilename = new System.Windows.Forms.TextBox();
+            this.textBoxFilesize = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.progressBarWrite = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorkerWrite = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filename:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Filesize:";
+            // 
+            // textBoxFilename
+            // 
+            this.textBoxFilename.Location = new System.Drawing.Point(70, 24);
+            this.textBoxFilename.Name = "textBoxFilename";
+            this.textBoxFilename.Size = new System.Drawing.Size(178, 20);
+            this.textBoxFilename.TabIndex = 2;
+            this.textBoxFilename.Text = "M:\\test.txt";
+            // 
+            // textBoxFilesize
+            // 
+            this.textBoxFilesize.Location = new System.Drawing.Point(70, 69);
+            this.textBoxFilesize.Name = "textBoxFilesize";
+            this.textBoxFilesize.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFilesize.TabIndex = 3;
+            this.textBoxFilesize.Text = "1024";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "MB";
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(173, 114);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 23);
+            this.buttonGo.TabIndex = 5;
+            this.buttonGo.Text = "GO!";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // progressBarWrite
+            // 
+            this.progressBarWrite.Location = new System.Drawing.Point(15, 114);
+            this.progressBarWrite.Name = "progressBarWrite";
+            this.progressBarWrite.Size = new System.Drawing.Size(152, 23);
+            this.progressBarWrite.TabIndex = 6;
+            // 
+            // backgroundWorkerWrite
+            // 
+            this.backgroundWorkerWrite.WorkerReportsProgress = true;
+            this.backgroundWorkerWrite.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerWrite_DoWork);
+            this.backgroundWorkerWrite.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorkerWrite_ProgressChanged);
+            this.backgroundWorkerWrite.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerWrite_RunWorkerCompleted);
+            // 
+            // AnnoyingFlooder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 155);
+            this.Controls.Add(this.progressBarWrite);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxFilesize);
+            this.Controls.Add(this.textBoxFilename);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "AnnoyingFlooder";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AnnoyingFlooder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFilename;
+        private System.Windows.Forms.TextBox textBoxFilesize;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.ProgressBar progressBarWrite;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerWrite;
+    }
+}
+
---
+++ AnnoyingFlooder/AnnoyingFlooder.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+using System.IO;
+
+namespace AnnoyingFlooder
+{
+    public partial class AnnoyingFlooder : Form
+    {
+        private enum FILE_ACCESS_RIGHTS : uint
+        {
+            GENERIC_READ = 0x80000000,
+            GENERIC_WRITE = 0x40000000
+        }
+
+        private enum FILE_FLAGS : uint
+        {
+            WriteThrough = 0x80000000,
+            Overlapped = 0x40000000,
+            NoBuffering = 0x20000000,
+            RandomAccess = 0x10000000,
+            SequentialScan = 0x8000000,
+            DeleteOnClose = 0x4000000,
+            BackupSemantics = 0x2000000,
+            PosixSemantics = 0x1000000,
+            OpenReparsePoint = 0x200000,
+            OpenNoRecall = 0x100000
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr CreateFile(string fileName, 
+            FILE_ACCESS_RIGHTS access, 
+            FileShare share, 
+            int securityAttributes, 
+            FileMode creation, 
+            FILE_FLAGS flags, 
+            IntPtr templateFile);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CloseHandle(IntPtr handle);
+
+        public AnnoyingFlooder()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            buttonGo.Enabled = false;
+            progressBarWrite.Minimum = 0;
+            progressBarWrite.Maximum = int.Parse(textBoxFilesize.Text);
+            backgroundWorkerWrite.RunWorkerAsync();
+        }
+
+        private void backgroundWorkerWrite_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Schreiben des Streams
+            IntPtr fileHandle = CreateFile(textBoxFilename.Text + ":hidden",
+                FILE_ACCESS_RIGHTS.GENERIC_WRITE,
+                FileShare.Write,
+                0,
+                FileMode.Create,
+                0,
+                IntPtr.Zero);
+
+            FileStream writer =
+                new FileStream(new SafeFileHandle(fileHandle, true), FileAccess.Write);
+
+            byte[] buffer = new byte[1048576];
+            for (int index = 0; index < int.Parse(textBoxFilesize.Text); index++)
+            {
+                writer.Write(buffer, 0, buffer.Length);
+                backgroundWorkerWrite.ReportProgress(index);
+            }
+
+            writer.Close();
+            CloseHandle(fileHandle);
+
+            MessageBox.Show("Done!", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void backgroundWorkerWrite_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBarWrite.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorkerWrite_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            buttonGo.Enabled = true;
+            progressBarWrite.Value = 0;
+        }
+
+        private void textBoxFilesize_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int isNumber = 0;
+            e.Handled = !int.TryParse(e.KeyChar.ToString(), out isNumber);
+        }
+    }
+}
